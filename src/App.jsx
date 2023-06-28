035dd41a1d496f5a6e7014f076bd21c7f3dbd71d
ygt-design <103063122+ygt-design@users.noreply.github.com>
2023-06-28 17:52:19 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { createSignal } from 'solid-js';
-import ChoiceCard from './components/Choice';
+import { createSignal } from "solid-js";
+import ChoiceCard from "./components/Choice";
 
 function App() {
   const [points, setPoints] = createSignal(0);
+  const [prevPoints, setPrevPoints] = createSignal(0); // New state variable to store previous points
   const [selectedOptions, setSelectedOptions] = createSignal([]);
   const [step, setStep] = createSignal(1);
   const [showErrorMessage, setShowErrorMessage] = createSignal(false);
@@ -21,11 +22,12 @@ function App() {
   };
 
   const handleConfirm = () => {
-    if (selectedOptions().length === 0) {
-      // Show error message if no option is selected
+    if (selectedOptions().length === 0 && step() !== 3) {
+      // Show error message if no option is selected (except in the final step)
       setShowErrorMessage(true);
     } else {
       const totalPoints = selectedOptions().reduce((sum, option) => sum + option, 0);
+      setPrevPoints(points()); // Store previous points before proceeding to the next step
       setPoints((prevPoints) => prevPoints + totalPoints);
       setSelectedOptions([]);
       setStep((prevStep) => prevStep + 1);
@@ -35,16 +37,30 @@ function App() {
 
   const handleReset = () => {
     setPoints(0);
+    setPrevPoints(0); // Reset previous points
     setSelectedOptions([]);
     setStep(1);
     setShowErrorMessage(false);
   };
 
+  const handleGoBack = () => {
+    setSelectedOptions([]);
+    setPoints(prevPoints()); // Set points to previous points
+    setPrevPoints(0); // Reset previous points
+    setStep((prevStep) => prevStep - 1);
+    setShowErrorMessage(false);
+  };
+
   return (
-    <div>
+    <div class="container">
       {step() === 1 && (
         <div>
           <p>Current Points: {points()}</p>
+          <ChoiceCard
+            value={0} // Add the option for 0 points
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
           <ChoiceCard
             value={1}
             selectedOptions={selectedOptions}
@@ -64,6 +80,11 @@ function App() {
         <div>
           <p>Current Points: {points()}</p>
           <ChoiceCard
+            value={0} // Add the option for 0 points
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          <ChoiceCard
             value={3}
             selectedOptions={selectedOptions}
             handleOptionClick={handleOptionClick}
@@ -75,10 +96,59 @@ function App() {
           />
           {showErrorMessage() && <p>Please choose an option to continue.</p>}
           <button onClick={handleConfirm}>Confirm</button>
+          {step() > 1 && <button onClick={handleGoBack}>Go Back</button>}
         </div>
       )}
 
       {step() === 3 && (
+        <div>
+          <p>Current Points: {points()}</p>
+          <ChoiceCard
+            value={0} // Add the option for 0 points
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          <ChoiceCard
+            value={3}
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          <ChoiceCard
+            value={4}
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          {showErrorMessage() && <p>Please choose an option to continue.</p>}
+          <button onClick={handleConfirm}>Confirm</button>
+          {step() > 1 && <button onClick={handleGoBack}>Go Back</button>}
+        </div>
+      )}
+
+      {step() === 4 && (
+        <div>
+          <p>Current Points: {points()}</p>
+          <ChoiceCard
+            value={0} // Add the option for 0 points
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          <ChoiceCard
+            value={3}
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          <ChoiceCard
+            value={4}
+            selectedOptions={selectedOptions}
+            handleOptionClick={handleOptionClick}
+          />
+          {showErrorMessage() && <p>Please choose an option to continue.</p>}
+          <button onClick={handleConfirm}>Confirm</button>
+          {step() > 1 && <button onClick={handleGoBack}>Go Back</button>}
+        </div>
+      )}
+
+      {step() === 5 && (
         <div>
           <p>Final Points: {points()}</p>
           <button onClick={handleReset}>Restart</button>
--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,13 +1,14 @@
-export default function ChoiceCard({ value, selectedOptions, handleOptionClick, showErrorMessage }) {
+export default function ChoiceCard({ value, selectedOptions, handleOptionClick, answer}) {
     return (
-      <div>
-        <button
-          onClick={() => handleOptionClick(value)}
-          classList={{ selected: selectedOptions().includes(value) }}
-        >
-          Choose {value}
-        </button>
-      </div>
+        <div>
+            <button
+            onClick={() => handleOptionClick(value)}
+            classList={{ selected: selectedOptions().includes(value) }}
+            >
+            Choose {value}
+            {answer}
+            </button>
+        </div>
     );
   }
   
